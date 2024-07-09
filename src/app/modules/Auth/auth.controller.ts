0ef9eb5278ce9f504fcab4381294931bf5dd469e
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-09 13:43:59 +0600

    [Implement] auth module

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,43 +1,43 @@
-// import { Request, Response } from 'express';
-// import { authService } from './auth.service';
-// import catchAsync from '../../utils/catchAsync';
-// import sendResponse from '../../utils/sendResponse';
-// import httpStatus from 'http-status';
-// import generateToken from '../../utils/jwt';
+import { Request, Response } from 'express';
+import { authService } from './auth.service';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import generateToken from '../../utils/jwt';
 
-// const userlogin = catchAsync(async (req: Request, res: Response) => {
-//   const { email, password } = req.body;
+const userlogin = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-//   const user = await authService.loginUser(email, password);
+  const user = await authService.loginUser(email, password);
 
-//   if (!user) {
-//     sendResponse(res, {
-//       success: false,
-//       statusCode: httpStatus.UNAUTHORIZED,
-//       message: 'Invalid email or password',
-//     });
-//   }
+  if (!user) {
+    sendResponse(res, {
+      success: false,
+      statusCode: httpStatus.UNAUTHORIZED,
+      message: 'Invalid email or password',
+    });
+  }
 
-//   // Generate JWT token
-//   const token = generateToken(user.email, user.role);
+  // Generate JWT token
+  const token = generateToken(user.email, user.role);
 
-//   // Return success response with token and user data
-//   sendResponse(res, {
-//     success: true,
-//     statusCode: httpStatus.OK,
-//     message: 'User logged in successfully',
-//     token: token,
-//     data: {
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       phone: user.phone,
-//       role: user.role,
-//       address: user.address,
-//     },
-//   });
-// });
+  // Return success response with token and user data
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'User logged in successfully',
+    token: token,
+    data: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+      address: user.address,
+    },
+  });
+});
 
-// export const authController = {
-//   userlogin,
-// };
+export const authController = {
+  userlogin,
+};
