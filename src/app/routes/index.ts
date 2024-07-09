174e7eeeacacc925f7b3436378f890119e55de60
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-09 14:43:41 +0600

    🚚 Centralized router setup for modular route handling

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,25 +1,25 @@
-// import { Router } from 'express';
-// import { ProductRoutes } from '../modules/Product/product.route';
-// import { UserRoutes } from '../modules/User/user.route';
-// import { AuthRoutes } from '../modules/Auth/auth.route';
+import { Router } from 'express';
+import { ProductRoutes } from '../modules/Product/product.route';
+import { UserRoutes } from '../modules/User/user.route';
+import { AuthRoutes } from '../modules/Auth/auth.route';
 
-// const router = Router();
+const router = Router();
 
-// const moduleRoutes = [
-//   {
-//     path: '/users',
-//     route: UserRoutes,
-//   },
-//   {
-//     path: '/auth',
-//     route: AuthRoutes,
-//   },
-//   {
-//     path: '/product',
-//     route: ProductRoutes,
-//   },
-// ];
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+  {
+    path: '/product',
+    route: ProductRoutes,
+  },
+];
 
-// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
-// export default router;
+export default router;
