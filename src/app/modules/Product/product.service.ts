7ff14ae6b7278d801e22863664f0285903d874fd
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-10 10:25:03 +0600

    💥 Add `getAllProducts` function to handle querying of products with advanced filtering, sorting, pagination, and field selection capabilities.

--- src/app/modules/Product/product.service.ts
+++ src/app/modules/Product/product.service.ts
@@ -1,4 +1,7 @@
+/* eslint-disable prefer-const */
+import QueryBuilder from '../../builder/QueryBuilder';
 import { Product } from './product..model';
+import { ProductSearchableFields } from './product.constant';
 import { IProduct } from './product.interface';
 
 const createProduct = async (payload: IProduct): Promise<IProduct> => {
@@ -6,8 +9,27 @@ const createProduct = async (payload: IProduct): Promise<IProduct> => {
   return product;
 };
 
-const getAllProducts = async (): Promise<IProduct[]> => {
-  return await Product.find({ isDeleted: false });
+// const getAllProducts = async (): Promise<IProduct[]> => {
+//   return await Product.find({ isDeleted: false });
+// };
+
+const getAllProducts = async (query: Record<string, unknown>) => {
+  const productQuery = new QueryBuilder(
+    Product.find({ isDeleted: false }),
+    query,
+  )
+    .search(ProductSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await productQuery.modelQuery;
+  const meta = await productQuery.countTotal();
+  return {
+    result,
+    meta,
+  };
 };
 
 const getSingleProduct = async (id: string): Promise<IProduct | null> => {
