712df20fc404c897cf1fcb855c45a1d6da18c572
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-31 14:59:00 +0600

    [Fixed] cors issue

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://mech-arcade-backend.vercel.app'],
+    origin: ['http://localhost:5173','https://mech-arcade-seven.vercel.app'],
     credentials: true,
   }),
 );
